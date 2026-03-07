f1a4140a12b07620cf728db2a5a4618503839880
Ninadsali05 <ninad2388@gmail.com>
2026-03-07 15:00:38 +0530

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -8,13 +8,13 @@ export const projects = {
       title: "DFMEA Moderation",
       description: "Developed and maintained DFMEA as per European standards for automotive systems.",
       tags: ["DFMEA", "SCIO/e1ns", "Automotive"],
-      image: src="/Profile-NS/DFMEA.png"
+      image: "/Profile-NS/DFMEA.png"
     },
     {
       title: "CAD Support & Series Activities.",
       description: "Supported CAD modelling & drawings for ECU plastic parts. Also worked on PCBA technology,",
       tags: ["CREO", "ECU"],
-      image: src="/Profile-NS/CAD.png"
+      image: "/Profile-NS/CAD.png"
     }
   ],
   plm: [
@@ -22,13 +22,13 @@ export const projects = {
       title: "BMIDE Customization",
       description: "Customized Teamcenter data model by implementing Item Types, properties, LOVs, and naming rules.",
       tags: ["BMIDE", "Data Modeling", "Teamcenter"],
-      image: src="/Profile-NS/BMIDE.png"
+      image: "/Profile-NS/BMIDE.png"
     },
     {
       title: "Custom ITK utilities",
       description: "Developed custom ITK utilities for server-side automation and data management in Teamcenter.",
       tags: ["ITK", "C/C++", "PLM"],
-      image: src="/Profile-NS/ITK.png"
+      image: "/Profile-NS/ITK.png"
     }
   ],
   automation: [
@@ -36,13 +36,13 @@ export const projects = {
       title: "Portfolio Website",
       description: "Engineered an immersive digital experience by synergizing React with advanced AI-driven development workflows, pushing the boundaries of traditional portfolio design through creative automation and modern web aesthetics.",
       tags: ["React", "AI-Assisted Dev", "Creative Automation", "Framer Motion"],
-      image: src="/Profile-NS/Web.png"
+      image: "/Profile-NS/Web.png"
     },
     {
       title: "Confluence Knowledge Hub",
       description: "Established a centralized Confluence wiki to consolidate technical know-how and project documentation, significantly enhancing cross-team collaboration and information accessibility.",
       tags: ["Confluence", "Knowledge Management", "Collaboration"],
-      image: src="/Profile-NS/Wiki.png"
+      image: "/Profile-NS/Wiki.png"
     }
   ]
 };
