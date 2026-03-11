aa7f7f28363fd02666b6ac673b59e23fb6bf6edc
Ninadsali05 <ninad2388@gmail.com>
2026-03-11 11:06:49 +0530

    Update import for motion from framer-motion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'motion/react';
+import { motion } from 'framer-motion';
 import ThemeToggle from "./ThemeToggle";
 
 import { Tab } from '../types';
